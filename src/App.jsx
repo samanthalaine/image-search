654ddf7bff471427daa773fedcf65b4b722bf782
Samantha <samanthalaine8@gmail.com>
2025-10-28 19:32:11 -0400

    feat: load images and add search

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+import ImageCard from './components/ImageCard'
+import ImageSearch from './components/ImageSearch'
+
 
 function App() {
+  const [images, setImages] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [term, setTerm] = useState('')
+
+  useEffect(() => {
+    try {
+    const fetchImages = async () => {
+      const res = await fetch(`https://pixabay.com/api/?key=${import.meta.env.VITE_REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true&safesearch=true&per_page=21`)
+      const data = await res.json()
+      setImages(data.hits)
+      setIsLoading(false)
+    }
+    fetchImages()
+  } catch (err) {
+    console.log(err)
+  }
+  },[term])
+
 
   return (
-    <>
-      <h1 className="text-4xl font-bold text-indigo-600">Tailwind is working 🎉</h1>
-    </>
-  )
+    <div className="container mx-auto mb-10 mt-8">
+      <h1 className="text-center text-blue-700 text-3xl font-bold">ImageFinder</h1>
+      <h2 className="text-center text-gray-400 mt-2">Find beautiful, high-quality photos.</h2>
+      <ImageSearch searchText={(text)=>setTerm(text)}/>
+
+      {!isLoading && images.length === 0 && <h1 className="text-3xl text-gray-500 text-center mx-auto mt-15">No results found. Please try another search.</h1>}
+
+      {isLoading ? <h1 className="text-4xl text-center text-gray-500 mx-auto mt-32">Loading...</h1> : <div className="sm:grid grid-cols-1 md:grid grid-cols-3 gap-7">
+        {images.map((image)=>(
+          <ImageCard key={image.id} image={image}/>
+        ))}
+      </div>}
+    </div>
+  );
 }
 
 export default App
---
+++ src/components/ImageCard.jsx
@@ -0,0 +1,36 @@
+function ImageCard({image}) {
+  const tags = image.tags.split(',')
+
+    return (
+        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <img src={image.webformatURL} alt={image.webformatURL} className="w-full"/>
+      <div className="px-6 py-3">
+      <div className="font-semibold text-blue-700 text-xl mb-2">
+        Photo by {image.user}
+      </div>
+      <ul>
+        <li>
+          <strong>Views: </strong>
+          {image.views}
+        </li>
+        <li>
+          <strong>Downloads: </strong>
+          {image.downloads}
+        </li>
+        <li>
+          <strong>Likes: </strong>
+          {image.likes}
+        </li>
+      </ul>
+      </div>
+      <div className="px-6 py-3">
+        {tags.map((tag, index) =>(<span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-800 mr-2">
+          #{tag}
+        </span>))}
+       
+      </div>
+    </div>
+    )
+}
+
+export default ImageCard
